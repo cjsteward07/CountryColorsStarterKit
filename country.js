516dc8e95672122bd47b99324330a5652bbb9535
unknown <cjsteward07@gmail.com>
2023-02-09 18:10:35 -0500

    update

--- country.js
+++ country.js
@@ -1,29 +1,55 @@
-class Country {
-    constructor(name, lang, greeting, colors) {
+class Country 
+{
+    constructor(name, lang, greeting, colors)
+    {
         this.name = name;
         this.lang = lang;
         this.greeting = greeting;
         this.colors = colors;
     }
+
+    ChangeColors()
+    {
+        document.querySelector("#Color1").style.backgroundColor = this.colors[0];
+        document.querySelector("#Color2").style.backgroundColor = this.colors[1];
+        document.querySelector("#Color3").style.backgroundColor = this.colors[2];
+    }
 }
+
 let usa = new Country("USA", "Murican", "Why Hello there world! Have some McDonalds", ["red", "white", "blue"]);
 let mexico = new Country("Mexico", "Spanish", "Hola mundo!", ["red", "white", "green"]);
-let algeria = new Country("Algeria", "Arabic and Berber", "Sup", ["blue", "yellow", "red"])
+let ukraine = new Country("Ukraine", "Ukrainian", "Pryvit!", ["blue", "yellow", "blue"]);
+let algeria = new Country("Algeria", "Arabic and Berber", "Sup", ["blue", "yellow", "red"]);
+let canada = new Country("Canada", "English", "Hey bud!", ["white", "white", "red"]);
 
-
-function SwitchCountry() {
-    let input = "USA";
+function SwitchCountry() 
+{
+    let input = document.querySelector("#CountryList");
     let country;
 
-    if (input === "USA") {
-        //set country to usa 
+    if (input.value === "USA") 
+    {
         country = usa;
     }
-    else if (input === "Mexico") {
+    else if (input.value === "Mexico") 
+    {
         country = mexico;
     }
-    else if (input === "Algeria") {
+    else if (input.value === "Ukraine") 
+    {
+        country = ukraine;
+    }
+    else if (input.value === "Algeria") 
+    {
         country = algeria;
     }
+    else if (input.value === "Canada") 
+    {
+        country = canada;
+    }
 
+    country.ChangeColors();
+    document.querySelector("#CountryName").innerText = country.name;
+    document.querySelector("#OfficialLanguage").innerText = (`Language: ${country.lang}`);
+    document.querySelector("#HelloWorld").innerText = country.greeting;
 }
